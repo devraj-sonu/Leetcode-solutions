0ad7421695f5c083035e429aa0b4048fff0c9b87
Devraj Giri <devraj6477@gmail.com>
2025-09-21 18:08:05 +0530

    Add remove elem file in repo

---
+++ array sol/remove elements/remove elements.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        n = len(nums)
+
+        k = 0
+        for i in range(0,n):
+            if(nums[i] != val) :
+                nums[k] = nums[i]
+                k += 1
+        return k
