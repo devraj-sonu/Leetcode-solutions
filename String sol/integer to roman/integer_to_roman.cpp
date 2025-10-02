505cb753b24a0ffd8179f4c42c549f89535d4b45
Devraj Giri <devraj6477@gmail.com>
2025-10-02 14:56:22 +0530

    Add integer to roman files in my github repo

--- String sol/integer to roman/integer_to_roman.cpp
+++ String sol/integer to roman/integer_to_roman.cpp
@@ -1,11 +1,57 @@
 #include<iostream>
+#include<vector>
+#include<string>
+
 using namespace std;
 
-int main() {
-cout<<"Hello world";
+class Solution {
+public:
+    string inttoRoman(int num) {
+        static vector<int> val{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        static vector<string> sym{"M", "CM", "D", "XL", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
 
-    return 0;
-}
+        string result = "";
+
+        for(int i=0; i<13; i++) {
+            if(num == 0) {
+                break;
+
+                int times = num/val[i];
+
+                while(times --) {
+                    result += sym[i];
+                }
+
+                num = num % val[i];
+            }
+        }
+        return result;
+    }
+
+};
 
 
-return "phaltu chele"
+
+
+// Accurate solution
+
+
+
+
+class Solution {
+public:
+    string intToRoman(int num) {
+        int val[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string sym[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+        string result = "";
+        for(int i=0; i<13; i++) {
+            while(val[i] > num) {
+                result += sym[i];
+                num -= val[i];
+            }
+        }
+        return result;
+    }
+
+};
