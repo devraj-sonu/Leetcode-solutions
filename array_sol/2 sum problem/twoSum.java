7512aee81d22a52c77873b77c531611601383844
devraj-sonu <devraj6477@gmail.com>
2026-08-16 08:09:02 +0530

    add 2 sum leetcode problem solution in my repo

---
+++ array_sol/2 sum problem/twoSum.java
@@ -0,0 +1,26 @@
+import java.util.HashMap;
+import java.util.Arrays;
+
+public class twoSum {
+    public static int[] twosum(int[] nums, int target) {
+        HashMap<Integer,Integer>map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++) {
+            int needMore = target - nums[i];
+            if(map.containsKey(needMore)) {
+                return new int[]{map.get(needMore),i};
+            }
+            map.put(nums[i],i);
+        }
+        return new int[]{};
+    }
+
+
+    public static void main(String[]args) {
+        int nums[] = {2,7,11,15};
+        int target = 9;
+        int[] result = twosum(nums, target);
+        System.out.println(Arrays.toString(result));
+    }
+
+}
